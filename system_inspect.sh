c9ed8989a4a47106197667c2e0254dd3e054a159
Daniel Robinson <ubuntu@ip-172-31-37-72.us-east-2.compute.internal>
2019-07-11 17:28:56 +0000

    initial progress of system_inspect.sh

---
+++ system_inspect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Define separator function
+separator () { echo "================================"; }
+
+separator
+systemctl status ntpd
+systemctl status sshd
+systemctl status snmpd
+
+separator
+yum install cowsay
+yum info cowsay
+
+separator
+
